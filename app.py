0e89b69ea6e08105ed9b658092c9afa58f80c7ee
diogo90 <96832561+diogo90@users.noreply.github.com>
2023-01-04 16:29:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -66,13 +66,7 @@ def main():
 
 	if choice == "Home":
 		st.subheader("Scope")
-		st.text("Self-development project focused on gaining practical understanding of building a recommendation system using cosine similarity\n
-		and deploying it with Streamlit. The aim is to develop an approach to find similar organisations based on their profile description on Crunchbase.\n
-		The short profile organizations’ descriptions tend to be focused on the core business model of a company and the market they are addressing.\n
-		The premise lies on the fact that if any two organizations are operating within the same market space and doing the same things, they will\n
-		use similar industry vocabulary to describe what they do. Which will consequently lead to a high cosine similarity between them.\n
-		To source organizations names and their description, I have used the Crunchbase API (details on Data Collection). Also, to limit the scope\n
-		I have only collected data on organizations headquartered in London, UK.")
+		st.text("Self-development project focused on gaining practical understanding of building a recommendation system using cosine similarity and deploying it with Streamlit. The aim is to develop an approach to find similar organisations based on their profile description on Crunchbase. The short profile organizations’ descriptions tend to be focused on the core business model of a company and the market they are addressing. The premise lies on the fact that if any two organizations are operating within the same market space and doing the same things, they will use similar industry vocabulary to describe what they do. Which will consequently lead to a high cosine similarity between them. To source organizations names and their description, I have used the Crunchbase API (details on Data Collection). Also, to limit the scope I have only collected data on organizations headquartered in London, UK.")
 		st.text("Below is a preview of the data sourced using the Crunchbase API (as off Nov. 22):")
 		st.dataframe(df.head(5))
 		st.text("Please head to the Recommender tab to test the tool")
@@ -101,8 +95,7 @@ def main():
 						st.write("Score: ", rec_score)
 
 				except:
-					result = "Sorry, can't find similar Organizations to this one. Most likely because it was \
-					not listed on Crunchbase as an organization headquartered in London as off Nov. 2022"
+					result = "Sorry, can't find similar Organizations to this one. Most likely because it was not listed on Crunchbase as an organization headquartered in London as off Nov. 2022"
 					st.write(result)
 
 
